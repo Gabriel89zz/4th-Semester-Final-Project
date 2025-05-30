c4e740bdbeb4848461ad04fc76d3aabb87ef09ff
Gabriel Torres <elcomparosh97@gmail.com>
2025-05-30 01:19:54 -0600

    .

--- 4th Semester Final Project/Form1.cs
+++ 4th Semester Final Project/Form1.cs
@@ -847,31 +847,6 @@ namespace _4th_Semester_Final_Project
             return movies;
         }
 
-        public class TmdbPopularResponse
-        {
-            public int page { get; set; }
-            public int total_pages { get; set; }
-            public List<Movie> results { get; set; }
-        }
-
-        public class Movie
-        {
-            public int id { get; set; }
-            public bool adult { get; set; }
-            public string backdrop_path { get; set; }
-            public List<int> genre_ids { get; set; }
-            public string original_language { get; set; }
-            public string original_title { get; set; }
-            public string overview { get; set; }
-            public double popularity { get; set; }
-            public string poster_path { get; set; }
-            public DateTime release_date { get; set; }
-            public string title { get; set; }
-            public bool video { get; set; }
-            public double vote_average { get; set; }
-            public int vote_count { get; set; }
-        }
-
         private void btnBD_Click(object sender, EventArgs e)
         {
             try
---
+++ 4th Semester Final Project/TmdbPopularResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4th_Semester_Final_Project
+{
+    internal class TmdbPopularResponse
+    {
+        public int page { get; set; }
+        public int total_pages { get; set; }
+        public List<Movie> results { get; set; }
+    }
+}
